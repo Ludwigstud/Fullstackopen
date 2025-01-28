845d054eb86d7887fb92cad59bcc718d6522c178
ludwig <ludwigstridh@gmail.com>
2025-01-28 14:15:33 +0100

    finished with 2.4

--- part2/a/part2/src/App.jsx
+++ part2/a/part2/src/App.jsx
@@ -1,50 +1,80 @@
-const Course = ({ info}) => {
-	return <li>{info.name}: {info.exercises}</li>;
+const Course = ({ info }) => {
+	return (
+		<li>
+			{info.name}: {info.exercises}
+		</li>
+	);
 };
 
 const App = () => {
-	const course = {
-		id: 1,
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-				id: 1,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-				id: 2,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-				id: 3,
-			},
-		],
-	};
+	const courses = [
+		{
+			name: "Half Stack application development",
+			id: 1,
+			parts: [
+				{
+					name: "Fundamentals of React",
+					exercises: 10,
+					id: 1,
+				},
+				{
+					name: "Using props to pass data",
+					exercises: 7,
+					id: 2,
+				},
+				{
+					name: "State of a component",
+					exercises: 14,
+					id: 3,
+				},
+				{
+					name: "Redux",
+					exercises: 11,
+					id: 4,
+				},
+			],
+		},
+		{
+			name: "Node.js",
+			id: 2,
+			parts: [
+				{
+					name: "Routing",
+					exercises: 3,
+					id: 1,
+				},
+				{
+					name: "Middlewares",
+					exercises: 7,
+					id: 2,
+				},
+			],
+		},
+	];
 
-	const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+	return (
+		<>
+			{courses.map((course) => {
+				const totalEx = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+				return (
+					<div key={course.id}>
+						<h1> {course.name}</h1>
+						<p>
+							{course.parts.map((part) => (
+								<Course
+									key={part.id}
+									info={part}
+								/>
+							))}
+						</p>
+						<p>Total of {totalEx} excercises</p>
+					</div>
+				);
+			})}
 
-
-	return(
-<>
-<h1>{course.name}</h1>
-
-<ul>
-{course.parts.map((part) => (
-	<Course key={part.id} info={part} />
-	
-))}
-</ul>
-<p>Total number of excerises: {totalExercises}</p>
-
-
-</>
-
-
-	)
+			
+		</>
+	);
 };
 
 export default App;
