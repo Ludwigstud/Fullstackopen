5c8fd8449544532033ab0a84f0791a93462326f7
ludwig <ludwigstridh@gmail.com>
2025-01-28 17:17:06 +0100

    done with Forms studies, now starting on excercise 2.6

--- part2/a/part2/src/App.jsx
+++ part2/a/part2/src/App.jsx
@@ -1,72 +1,53 @@
 import { useState } from "react";
-import Course from "./components/Course";
+import Note from "./components/Note";
 
-const App = () => {
-	const courses = [
-		{
-			name: "Half Stack application development",
-			id: 1,
-			parts: [
-				{
-					name: "Fundamentals of React",
-					exercises: 10,
-					id: 1,
-				},
-				{
-					name: "Using props to pass data",
-					exercises: 7,
-					id: 2,
-				},
-				{
-					name: "State of a component",
-					exercises: 14,
-					id: 3,
-				},
-				{
-					name: "Redux",
-					exercises: 11,
-					id: 4,
-				},
-			],
-		},
-		{
-			name: "Node.js",
-			id: 2,
-			parts: [
-				{
-					name: "Routing",
-					exercises: 3,
-					id: 1,
-				},
-				{
-					name: "Middlewares",
-					exercises: 7,
-					id: 2,
-				},
-			],
-		},
-	];
+const App = (props) => {
+	const [notes, setNotes] = useState(props.notes);
+	const [newNote, setNewNote] = useState("a new note...");
+	const [showAll, setShowAll] = useState(true);
+
+	const addNote = (event) => {
+		event.preventDefault();
+		const noteObject = {
+			content: newNote,
+			important: Math.random() < 0.5,
+			id: String(notes.length + 1),
+		};
+
+		setNotes(notes.concat(noteObject));
+		setNewNote("");
+	};
+
+	const handleNoteChange = (event) => {
+		console.log(event.target.value);
+		setNewNote(event.target.value);
+	};
+
+	const notesToShow = showAll ? notes : notes.filter((note) => note.important === true);
 
 	return (
-		<>
-			{courses.map((course) => {
-				const totalEx = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-				return (
-					<div key={course.id}>
-						<h1> {course.name}</h1>
-						<p>
-							{course.parts.map((part) => (
-								<Course
-									key={part.id}
-									info={part}
-								/>
-							))}
-						</p>
-						<p>Total of {totalEx} excercises</p>
-					</div>
-				);
-			})}
-		</>
+		<div>
+			<h1>Notes</h1>
+			<div>
+				<button onClick={() => setShowAll(!showAll)}>show {showAll ? "important" : "all"}</button>
+			</div>
+			<ul>
+				{notesToShow.map((note) => (
+					<Note
+						key={note.id}
+						note={note}
+					/>
+				))}
+			</ul>
+
+			<form onSubmit={addNote}>
+				<input
+					value={newNote}
+					onChange={handleNoteChange}
+				/>
+				<button type="submit">Save</button>
+			</form>
+		</div>
 	);
 };
 
